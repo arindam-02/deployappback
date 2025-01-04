7d2416be1f1672a581583ed8b135bd196a8cbbf1
arindam-02 <das.arin.ece@gmail.com>
2025-01-05 03:51:20 +0530

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/", (req, res) => {
   res.status(200).send({ status: true });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
